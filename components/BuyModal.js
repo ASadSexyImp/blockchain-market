d12442b42e4b3cef4ae73e718f03d49e76a5bb9c
ASadSexyImp <masakazuplus@gmail.com>
2022-08-01 09:09:25 +0900

    success on server env

--- components/BuyModal.js
+++ components/BuyModal.js
@@ -6,7 +6,7 @@ import Link from 'next/link'
 
 const BuyModal = ({close}) => {
     const styles = {
-        container: `h-full w-full flex flex-col `,
+        container: `h-full w-full flex flex-col`,
         closeX: `w-full h-[50px] flex items-center justify-end mb-[20px]`,
         title: `text-3xl font-bold flex flex-1 items-center mt-[20px] justify-center mb-[40px]`,
         content: `flex w-full mb-[30px] text-xl justify-center`,
@@ -18,7 +18,7 @@ const BuyModal = ({close}) => {
         etherscan: `w-full h-full flex items-center justify-center text-green-500 text-2xl mt-[20px] font-bold cursor-pointer`,
         success: `w-full h-full flex items-center justify-center text-xl mt-[20px] font-bolder`,
     }
-    
+
     const {
         amountDue,
         setAmountDue,
@@ -51,58 +51,58 @@ const BuyModal = ({close}) => {
                 </>
             ) : (
                 <>
-                <div className={styles.closeX}>
-                    <IoIosClose
-                    onClick={() => {
-                        close()
-                        setAmountDue('')
-                        setTokenAmount('')
-                        setEtherscanLink('')
-                    }}
-                    fontSize={50}
-                    className='cursor-pointer'
-                    />
-                </div>
-                <div className={styles.title}>Buy More Amazon Coins Here!</div>
-                <div className={styles.content}>
-                    Select how many tokens you would like to buy.
-                </div>
-                <div className={styles.input}>
-                    <input
-                    type='text'
-                    placeholder='Amount...'
-                    className={styles.inputBox}
-                    onChange={e => setTokenAmount(e.target.value)}
-                    value={tokenAmount}
-                    />
-                </div>
-                <div className={styles.price}>
-                    Total Due:{' '}
-                    {tokenAmount && tokenAmount > 0 ? amountDue + 'ETH' : '0 ETH'}
-                </div>
-                <button
-                    className={styles.buyBtn}
-                    disabled={!tokenAmount || tokenAmount < 0}
-                    onClick={() => {
-                    setIsLoading(true)
-                    buyTokens()
-                    }}
-                >
-                    Buy
-                </button>
-                {etherscanLink && (
-                    <>
-                    <div className={styles.success}>
-                        Transaction Sucessful! Check out your receipt for your
-                        transaction below!
+                    <div className={styles.closeX}>
+                        <IoIosClose
+                        onClick={() => {
+                            close()
+                            setAmountDue('')
+                            setTokenAmount('')
+                            setEtherscanLink('')
+                        }}
+                        fontSize={50}
+                        className='cursor-pointer'
+                        />
                     </div>
-                    <Link href={`${etherscanLink}`} className={styles.etherscan}>
-                        <a className={styles.etherscan} target='_blank'>
-                        Transaction Receipt
-                        </a>
-                    </Link>
-                    </>
-                )}
+                    <div className={styles.title}>Buy More Amazon Coins Here!</div>
+                    <div className={styles.content}>
+                        Select how many tokens you would like to buy.
+                    </div>
+                    <div className={styles.input}>
+                        <input
+                        type='text'
+                        placeholder='Amount...'
+                        className={styles.inputBox}
+                        onChange={e => setTokenAmount(e.target.value)}
+                        value={tokenAmount}
+                        />
+                    </div>
+                    <div className={styles.price}>
+                        Total Due:{' '}
+                        {tokenAmount && tokenAmount > 0 ? amountDue + 'ETH' : '0 ETH'}
+                    </div>
+                    <button
+                        className={styles.buyBtn}
+                        disabled={!tokenAmount || tokenAmount < 0}
+                        onClick={() => {
+                        setIsLoading(true)
+                        buyTokens()
+                        }}
+                    >
+                        Buy
+                    </button>
+                    {etherscanLink && (
+                        <>
+                            <div className={styles.success}>
+                                Transaction Sucessful! Check out your receipt for your
+                                transaction below!
+                            </div>
+                            <Link href={`${etherscanLink}`} className={styles.etherscan}>
+                                <a className={styles.etherscan} target='_blank'>
+                                Transaction Receipt
+                                </a>
+                            </Link>
+                        </>
+                    )}
                 </>
             )}
         </div>
